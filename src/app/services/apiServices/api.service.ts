2fd8b6c7305e2878df75635dee41714b66bd3b15
Mahesh Nagendra Kumar <68604040+Maheshkmnk@users.noreply.github.com>
2023-08-21 12:08:00 +0530

    Update api.service.ts

--- src/app/services/apiServices/api.service.ts
+++ src/app/services/apiServices/api.service.ts
@@ -9,8 +9,8 @@ export class ApiService {
 
   constructor(private http:HttpClient) { }
   getData(city:string):Observable<any>{
-  // let  UrlForWeather1= `http://api.weatherstack.com/current?access_key=aa5492b10be3fdd630b99627a22f3f9f&query=${city}`
-   let UrlForWeather =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=aa77353d74f6b87fa51eae1d5e4e2576&units=metric`
+  // let  UrlForWeather1= `http://api.weatherstack.com/current?access_key=placeYourKeyHere&query=${city}`
+   let UrlForWeather =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid="placeYourKeyHere"&units=metric`
     return this.http.get(UrlForWeather);
     return this.http.get('assets/user.json');
   }
